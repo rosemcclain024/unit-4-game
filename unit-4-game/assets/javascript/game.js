f2c4aa3ae52af5224b9947b69776f1d9d4ef70df
Rose McClain <rosemcclain024@gmail.com>
2019-03-05 21:07:39 -0500

    logging wins and losses in the console. Working towards resetting the game and putting making everything visible on the page

---
+++ unit-4-game/assets/javascript/game.js
@@ -0,0 +1,75 @@
+
+
+
+
+var randomResult;
+var loss;
+var win;
+var previous = 0;
+// setters
+// getters
+
+//$(".crystal").attr('class');
+
+randomResult = Math.floor(Math.random() * 120) + 19;
+
+
+$("#result").html('Random Result: ' + randomResult);
+
+for (var i=0; i<4; i++) {
+
+    var randomNumber = Math.floor(Math.random() * 11) + 1;
+    console.log(randomNumber);
+    
+    var crystal = $("<div>");
+        crystal.attr({ 
+            "class":  'crystal', 
+            "data-random": randomNumber
+    });
+
+    crystal.html(randomNumber);
+    
+    $(".crystals").append(crystal);
+    //console.log(randomNumber);
+
+}
+
+$(".crystal").on('click',function() {
+
+
+
+    var num = parseInt($(this).attr('data-random')); 
+    //var num = ($(this).attr('data-random'));
+
+    previous += num;
+    console.log(previous);
+
+    if (previous > randomResult) {
+        console.log("You lose :(");
+    }
+    else if (previous === randomResult) {
+        console.log("You won!");
+    }
+
+    if (randomNumber === randomResult) {
+        
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
