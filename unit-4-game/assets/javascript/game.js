ea09f32933cc65ac2f1db9eec5eba3238daa7d86
Rose McClain <rosemcclain024@gmail.com>
2019-03-05 22:18:49 -0500

    further progress

--- unit-4-game/assets/javascript/game.js
+++ unit-4-game/assets/javascript/game.js
@@ -1,10 +1,11 @@
 
 
-
+var randomNumber;
+var crystal;
 
 var randomResult;
-var loss;
-var win;
+var loss = 1;
+var win = 1;
 var previous = 0;
 // setters
 // getters
@@ -16,29 +17,60 @@ randomResult = Math.floor(Math.random() * 120) + 19;
 
 $("#result").html('Random Result: ' + randomResult);
 
-for (var i=0; i<4; i++) {
+for (var i = 0; i < 4; i++) {
 
-    var randomNumber = Math.floor(Math.random() * 11) + 1;
+    randomNumber = Math.floor(Math.random() * 11) + 1;
     console.log(randomNumber);
-    
-    var crystal = $("<div>");
-        crystal.attr({ 
-            "class":  'crystal', 
-            "data-random": randomNumber
+
+    crystal = $("<div>");
+    crystal.attr({
+        "class": 'crystal '+i,
+        "data-random": randomNumber
     });
 
     crystal.html(randomNumber);
-    
+
     $(".crystals").append(crystal);
     //console.log(randomNumber);
 
 }
 
-$(".crystal").on('click',function() {
 
 
 
-    var num = parseInt($(this).attr('data-random')); 
+
+function reset() {
+    randomResult = Math.floor(Math.random() * 120) + 19;
+
+    $("#result").html('Random Result: ' + randomResult);
+
+    for (var i = 0; i < 4; i++) {
+
+        randomNumber = Math.floor(Math.random() * 11) + 1;
+        console.log(randomNumber);
+        $("."+i).attr({
+         "data-random": randomNumber
+        }
+        )
+        $("."+i).html(randomNumber);
+    
+        crystal.html(randomNumber);
+    
+        $(".crystals").append(crystal);
+        //console.log(randomNumber);
+    
+    }
+
+    previous = 0;
+
+}
+
+
+$(".crystal").on('click', function () {
+
+
+
+    var num = parseInt($(this).attr('data-random'));
     //var num = ($(this).attr('data-random'));
 
     previous += num;
@@ -46,13 +78,20 @@ $(".crystal").on('click',function() {
 
     if (previous > randomResult) {
         console.log("You lose :(");
+        $(".loss").html("Loss: " + loss++);
+        reset();
+
     }
     else if (previous === randomResult) {
         console.log("You won!");
+        $(".win").html("Win: " + win++);
+        reset();
+
     }
 
+
     if (randomNumber === randomResult) {
-        
+
     }
 });
 
